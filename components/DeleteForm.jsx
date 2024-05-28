e1d952b2bf6ee5ddbe5d24e290eff2475e1e9711
robbie-carragher <orcafinweb@gmail.com>
2024-05-27 21:01:07 -0700

    Pending state added to the submit button on delete

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,10 +1,21 @@
+'use client';
+import { useFormStatus } from 'react-dom';
 import { deleteTask } from '@/utils/actions';
+
+const SubmitBtn = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button type='submit' className='btn btn-xs btn-error' disabled={pending}>
+      {pending ? 'pending...' : 'delete'}
+    </button>
+  );
+};
 
 const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type='hidden' name='id' value={id} />
-      <button className='btn btn-error btn-xs'>delete</button>
+      <SubmitBtn />
     </form>
   );
 };

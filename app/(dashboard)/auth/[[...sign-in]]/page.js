d0522aa1fafd06e206f6aa6815a00c10a74a1c44
robbie-carragher <orcafinweb@gmail.com>
2024-06-14 17:43:35 -0700

    checking each file for error

--- app/(dashboard)/auth/[[...sign-in]]/page.js
+++ app/(dashboard)/auth/[[...sign-in]]/page.js
@@ -1,7 +1,6 @@
-import React from 'react'
 const SignInPage = ({ params }) => {
-    console.log(params);
-    return <h1 className='text-7xl'>SignInPage</h1>;
-  };
-  export default SignInPage;
+  console.log(params);
+  return <h1 className='text-7xl'>SignInPage</h1>;
+};
+export default SignInPage;
   
